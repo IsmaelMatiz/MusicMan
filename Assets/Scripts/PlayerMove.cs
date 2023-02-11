88a6bed00354dd88bb77f6712805f224ce1775b8
IsmaelMatiz <ismaelmatiz@gmail.com>
2023-02-11 00:08:02 -0500

    the guitar now shoot musical notes and cinemachine has been added

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -9,6 +9,11 @@ public class PlayerMove : MonoBehaviour
     private Vector3 fingerPosition;
     [SerializeField] private Camera mainCamera;
     private Vector3 lookAtDirection;
+
+    public GameObject bulletObj;
+    public Transform bulletSpawnPoint;
+    public float bulletSpeed;
+
     // Update is called once per frame
     void Update()
     {
@@ -20,7 +25,17 @@ public class PlayerMove : MonoBehaviour
 
             lookAtDirection = fingerPosition - target.position;
             target.right = lookAtDirection;
+
+            
+            if (fingerPlayer.phase == TouchPhase.Ended) {
+                Debug.Log("FIREEE!!");
+                var bullet = Instantiate(bulletObj, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+                bullet.GetComponent<Rigidbody2D>().velocity = bulletSpawnPoint.right * bulletSpeed;
+            }
+
         }
 
     }
+
+
 }
